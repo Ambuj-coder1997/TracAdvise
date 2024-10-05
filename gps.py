ca9d009b3b45f6fc8c4124ef73b1bfa22047ae1b
Ambuj-coder1997 <96371080+Ambuj-coder1997@users.noreply.github.com>
2024-10-05 22:05:33 +0530

    Update gps.py

--- gps.py
+++ gps.py
@@ -5,27 +5,26 @@ from datetime import datetime, timedelta
 import folium
 from streamlit_folium import folium_static
 import matplotlib.pyplot as plt
-from common import generate_engine_speed, generate_throttle_setting, generate_implement_depth, generate_actual_forward_speed, generate_latitude, generate_longitude
+#from common import generate_engine_speed, generate_throttle_setting, generate_implement_depth, generate_actual_forward_speed, generate_latitude, generate_longitude
 
 # Function to generate parameters within given ranges
-#def generate_engine_speed():
-    #return random.randint(1200, 1800)
+def generate_engine_speed():
+    return random.randint(1200, 1800)
 
-#def generate_throttle_setting():
-   # return random.randint(45, 85)
+def generate_throttle_setting():
+    return random.randint(45, 85)
 
-#def generate_implement_depth():
-    #return round(random.uniform(5, 45),2)
+def generate_implement_depth():
+    return round(random.uniform(5, 45),2)
 
-#def generate_actual_forward_speed():
-    #return round(random.uniform(0.8, 4.5), 2)
+def generate_actual_forward_speed():
+    return round(random.uniform(0.8, 4.5), 2)
 
-# Generate latitude and longitude
-#def generate_latitude(current_lat):
-   # return current_lat + random.uniform(-0.0001, 0.0001)
+def generate_latitude(current_lat):
+    return current_lat + random.uniform(-0.0001, 0.0001)
 
-#def generate_longitude(current_long):
-    #return current_long + random.uniform(-0.0001, 0.0001)
+def generate_longitude(current_long):
+    return current_long + random.uniform(-0.0001, 0.0001)
 
 # Icon URLs for the table
 icon_url = {
